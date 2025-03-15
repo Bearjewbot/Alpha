d046829a1a3d90cf51f5f386721ef31c43c6f34d
Daniel Lovened <daniel_lovened@hotmail.com>
2025-03-15 11:03:47 +0100

    cleanup files, add entities

---
+++ Data/Entities/ProjectEntity.cs
@@ -0,0 +1,26 @@
+namespace Data.Entities;
+
+public class ProjectEntity
+{
+    
+}
+
+public class TimetableEntity
+{
+    
+}
+
+public class CustomerEntity
+{
+    
+}
+
+public class BudgetEntity
+{
+    
+}
+
+public class StatusTypeEntity
+{
+    
+}
