48ad7feecb0be56edb814693bdcdc8d3b2b3d78c
Daniel Lovened <daniel_lovened@hotmail.com>
2025-03-25 15:27:45 +0100

    add list to webapp

--- Presentation.WebApp/Pages/Index.cshtml.cs
+++ Presentation.WebApp/Pages/Index.cshtml.cs
@@ -1,18 +1,37 @@
-using Microsoft.AspNetCore.Mvc;
+using Business.Interfaces;
+using Business.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace Presentation.WebApp.Pages;
 
-public class IndexModel : PageModel
+public class IndexModel(IProjectService projectService) : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    private readonly IProjectService _projectService = projectService;
+
+    public IEnumerable<Project> ProjectsList { get; set; } = [];
+
+    public async Task OnGet()
     {
-        _logger = logger;
+        var projects = await _projectService.GetProjectsAsync();
+        ProjectsList =
+        [
+            ..projects.Select(x => new Project
+            {
+                Id = 0,
+                Name = null,
+                Description = null,
+                Customer = null,
+                Budget = null,
+                Status = null,
+                Dates = null
+            })
+        ];
     }
 
-    public void OnGet()
+    public void OnPost()
     {
+        
     }
 }
